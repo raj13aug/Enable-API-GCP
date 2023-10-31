bbadf68d79824f47deff727b1a5a268044f8d993
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-10-31 18:44:26 +0530

    Update apis.tf

--- apis.tf
+++ apis.tf
@@ -6,7 +6,7 @@ locals {
 
 resource "google_project_service" "enabled_apis" {
   project  = var.project_id
-  for_each = toset(locals.all_project_services)
+  for_each = toset(local.all_project_services)
   service  = each.key
 
   disable_on_destroy = false
